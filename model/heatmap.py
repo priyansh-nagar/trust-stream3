767a876850568822b7aef098fab2c901b3c5da0d
poiesis <97947981+priyansh-nagar@users.noreply.github.com>
2025-12-31 21:46:54 +0530

    Refactor cam_heatmap for Streamlit deployment
    Commented out import statements and modified cam_heatmap function to return the original image.

--- model/heatmap.py
+++ model/heatmap.py
@@ -1,9 +1,8 @@
-import numpy as np
-from pytorch_grad_cam import GradCAM
-from pytorch_grad_cam.utils.image import show_cam_on_image
+# import numpy as np
+# from pytorch_grad_cam import GradCAM
+# from pytorch_grad_cam.utils.image import show_cam_on_image
 
 def cam_heatmap(model, tensor, image):
-    cam = GradCAM(model=model, target_layers=[model.blocks[-1]])
-    grayscale = cam(input_tensor=tensor)[0]
-    img = np.array(image).astype(float)/255
-    return show_cam_on_image(img, grayscale, use_rgb=True)
+    # stub function for Streamlit deployment
+    # returns the original image without heatmap
+    return image
